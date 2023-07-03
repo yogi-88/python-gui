e6ed334e81c5d061aa947894de23d02aa336728e
Yogita Negi <y.negi@exchange-data.com>
2023-07-03 16:42:46 +0100

    Created button widget

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from tkinter import *
+from random import randint
 
 App =Tk()
-App.title('App')
-App.geometry('250x100')
-Display = Label(App, text='Application Window')
-Display.pack()
+App.title('Random Generator')
+App.geometry('350x100')
+
+# Display = Label(App, text='Application Window')
+# Display.pack()
+
+def show_msg():
+    msg = Label(App, text=randint(1,100))
+    msg.pack()
+B = Button(App, text='Generate', command=show_msg)
+B.pack()
 
 App.mainloop()
